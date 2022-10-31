8878de453221f20573243f044edd23df928451c6
cynthia obadha <63761562+cynthiaobadha@users.noreply.github.com>
2022-10-31 09:48:02 +0300

    Update loginForm.js

--- frontend/src/component/doctors/loginForm.js
+++ frontend/src/component/doctors/loginForm.js
@@ -1,38 +1,100 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Redirect, useHistory } from "react-router-dom";
+import { Container, Row, Col, Card, Form, CardHeader, CardBody, FormGroup, CardFooter, Button, Label, Input } from 'reactstrap'
+import axios from 'axios';
+import { AuthContext } from '../Auth/AuthContext';
 
-function LoginForm({ Login, error }) {
-    const [details, setDetails] = useState({name: "cynthia", email: "obadhacynthia@gmail.com", password:"cynthia667"});
-  
-    const submitHandler = e => {
-        e.preventDefault();
+const LoginForm = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [status, setStatus] = useState(0);
+	const { token, setToken, googleId, setGoogleId } = useContext(AuthContext);
+	const history = useHistory();
 
+	async function login() {
+		try {
+			const res = await axios.post(
+				`${process.env.REACT_APP_SERVER_URL}/doctors/login/`,
+				{
+					username: username,
+					password: password
+				}
+			);
+			setStatus(res.status);
 
-        
-        Login(details);
-    }
-  return (
-<form onSubmit={submitHandler}>
-    <div className="form-inner">
-        <h2>Doctor's Login</h2>
-        
-   {(error !== "") ? ( <div className="error">{error}</div>) : ""}
-        <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" name="name" id="cynthioa" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
-        </div>
-        <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+			const token = res.data.token;
 
-        </div>
-        <input type="submit" value="LOGIN" />
-    </div>
-</form>
-    )
+			if (res.status === 200) {
+				window.localStorage.setItem("token", token);
+
+				// Remove the googleId if it exisits in the local storage
+				window.localStorage.removeItem("googleId");
+				setGoogleId(null);
+				setToken(token);
+				history.push('/doctor');
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	if (token && !googleId) {
+		return <Redirect to="/doctor" />
+	}
+	return (
+		<Container className='text-center'>
+			<Row>
+				<Col lg={6} className='offset-lg-3 mt-5 '>
+					<Card>
+						<Form>
+							<CardHeader className=''>Welcome back, Doc</CardHeader>
+							<CardBody >
+								<FormGroup row>
+									<Label for='email' sm={3}>
+										Username
+									</Label>
+									<Col sm={9}>
+										<Input
+											type='text'
+											name='username'
+											id='username'
+											placeholder='provide your username'
+											onChange={(e) => setUsername(e.target.value)}
+										/>
+									</Col>
+								</FormGroup>
+								<FormGroup row>
+									<Label for='password' sm={3}>
+										Password
+									</Label>
+									<Col sm={9}>
+										<Input
+											type='password'
+											name='password'
+											id='password'
+											placeholder='your password here'
+											onChange={(e) => setPassword(e.target.value)}
+											onKeyPress={(target) => {
+												if (target.charCode === 13) {
+                          							login();
+                        					}
+											} }
+										/>
+									</Col>
+								</FormGroup>
+								{status === 201 && <p className="warning" style={{ color: "red", fontSize: "15px" }}>Wrong username or password! Please try again</p>}
+							</CardBody>
+							<CardFooter>
+								<Button block color="primary" onClick={login}>
+									Sign In
+								</Button>
+							</CardFooter>
+						</Form>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
-export default LoginForm
+export default LoginForm;
